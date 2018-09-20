b3d3015cb734e2728ba55de613832dc1efeac4ee
osmar01 <osmarj.frota@gmail.com>
2018-09-20 16:08:32 -0400

    Commit Inicial

---
+++ src/controller/AcessoFXMLController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author Junnio
+ */
+public class AcessoFXMLController implements Initializable {
+    
+  
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+       
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
